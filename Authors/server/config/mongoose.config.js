f7064eab1d9227f64f21f7c55e91c5f6ceb0b1f6
emilyjread <51672829+emilyjread@users.noreply.github.com>
2021-07-22 15:58:02 -0700

    Delete Authors directory

--- Authors/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb://localhost/authors", {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
-.then(()=>console.log("established connection with the DB"))
-.catch(err=> console.log("something went wrong connecting to the database", err))
